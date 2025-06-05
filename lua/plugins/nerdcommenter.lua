1d841ac236c305f4af809423c80331703eaebb01
Masanao Ohba <masanao.oba@dragon.jp>
2025-06-05 15:36:48 +0900

    add: comment-in/out toggle plugin

---
+++ lua/plugins/nerdcommenter.lua
@@ -0,0 +1,12 @@
+return {
+  "preservim/nerdcommenter",
+  keys = {
+    { "<C-/>", "<Plug>NERDCommenterToggle", mode = { "n", "v" }, desc = "Toggle comment" },
+  },
+  init = function()
+    -- NERDCommenter の基本オプション（必要なら）
+    vim.g.NERDDefaultAlign = 'left'
+    vim.g.NERDSpaceDelims = 1
+    vim.g.NERDCommentEmptyLines = 1
+  end,
+}
