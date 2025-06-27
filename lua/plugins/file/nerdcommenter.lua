7c4e5ee21d2d40f2c5105524446f191da3919bee
Masanao Ohba <masanao.oba@dragon.jp>
2025-06-27 13:51:28 +0900

    fix: comment mappings

--- lua/plugins/file/nerdcommenter.lua
+++ lua/plugins/file/nerdcommenter.lua
@@ -5,6 +5,7 @@ return {
   },
   init = function()
     -- NERDCommenter の基本オプション（必要なら）
+    vim.g.NERDCreateDefaultMappings = 0
     vim.g.NERDDefaultAlign = 'left'
     vim.g.NERDSpaceDelims = 1
     vim.g.NERDCommentEmptyLines = 1
